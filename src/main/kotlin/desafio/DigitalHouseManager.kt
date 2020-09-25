0ca3e3b896795cb7dd8e4b55a2459fc97e9c76f2
Allan Fernando <allanfarmelin2001@yahoo.com.br>
2020-09-25 16:49:20 -0300

    Parte G

---
+++ src/main/kotlin/desafio/DigitalHouseManager.kt
@@ -0,0 +1,8 @@
+package desafio
+
+class DigitalHouseManager {
+    val alunos = mutableListOf<Aluno>()
+    val professores = mutableListOf<Professor>()
+    val cursos = mutableListOf<Curso>()
+    val matriculas = mutableListOf<Matricula>()
+}
