7d1a1312a0f7444a0ec1eed559f425c7b4b908fa
Allan Fernando <allanfarmelin2001@yahoo.com.br>
2020-09-25 16:32:41 -0300

    Parte C

---
+++ src/main/kotlin/desafio/Professor.kt
@@ -0,0 +1,7 @@
+package desafio
+
+class Professor(val nome: String, val sobrenome: String, var tempoDeCasa: Int, val codigoDeProfessor: Int) {
+    override fun equals(other: Any?): Boolean {
+        return (other as? Professor)?.codigoDeProfessor == this.codigoDeProfessor
+    }
+}
