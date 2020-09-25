5b6ec7da7e657a64b34174f5256b38ccbd39c68d
Allan Fernando <allanfarmelin2001@yahoo.com.br>
2020-09-25 16:44:20 -0300

    Parte E

--- src/main/kotlin/desafio/Curso.kt
+++ src/main/kotlin/desafio/Curso.kt
@@ -1,6 +1,12 @@
 package desafio
 
-class Curso(val nome: String, val codigoDeCurso: Int) {
+class Curso(val nome: String, val codigoDeCurso: Int, val maximoDeAlunos: Int) {
+
+    lateinit var professorTitular: ProfessorTitular
+    lateinit var professorAdjunto: ProfessorAdjunto
+
+    val alunosMatriculados = mutableListOf<Aluno>()
+
     override fun equals(other: Any?): Boolean {
         return (other as? Curso)?.codigoDeCurso == this.codigoDeCurso
     }
