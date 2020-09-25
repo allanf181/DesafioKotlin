901c3bcb6218b15865bd749c7586bcede0b0a86c
Allan Fernando <allanfarmelin2001@yahoo.com.br>
2020-09-25 16:56:44 -0300

    Parte H

--- src/main/kotlin/desafio/Curso.kt
+++ src/main/kotlin/desafio/Curso.kt
@@ -10,4 +10,17 @@ class Curso(val nome: String, val codigoDeCurso: Int, val maximoDeAlunos: Int) {
     override fun equals(other: Any?): Boolean {
         return (other as? Curso)?.codigoDeCurso == this.codigoDeCurso
     }
+
+    fun adicionarUmAluno(umAluno: Aluno): Boolean {
+        return if (alunosMatriculados.find { it == umAluno } == null && alunosMatriculados.size <= maximoDeAlunos){
+            alunosMatriculados.add(umAluno)
+            true
+        } else {
+            false
+        }
+    }
+
+    fun excluirAluno(umAluno: Aluno){
+        alunosMatriculados.remove(umAluno)
+    }
 }
